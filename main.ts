40dcc64e3336ec04b10fe9f655fd666fa0f6c210
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-01 20:21:28 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
-    for (let Index = 0; Index <= 11; Index++) {
-        matrix.hour_mark(Index, 25, 23, 20, 23)
-    }
-    matrix.writeClock_radius24(25, 23)
-    matrix.hour_mark(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), 25, 23, 0, 10)
-    matrix.minute_mark(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC), 25, 23, 0, 16)
-    matrix.writeDisplay()
+    rtcpcf85063tp.writeDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde), [0, -1 + rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC)])
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
-    matrix.writeDigitImageArray(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), 0, 48, 10, 0, matrix.eTransparent.u, matrix.oled_eFaktor(matrix.eFaktor.f2))
-    matrix.writeDisplay()
+    rtcpcf85063tp.writeDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde), [0, 1 + rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC)])
 })
-matrix.init(matrix.ePages.y64, true, false)
-matrix.writeDisplay()
+matrix.init(matrix.ePages.y64, false, false)
+matrix.writeDisplay(0, 7, matrix.eI2C.I2C_x3C)
 rtcpcf85063tp.beimStart(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
+loops.everyInterval(1000, function () {
+    rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
+    if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute))) {
+        matrix.clearMatrix(0, 5)
+        matrix.writeClock_radius24(30, 23, rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC))
+        matrix.writeDisplay(0, 5, matrix.eI2C.I2C_x3C)
+    }
+    matrix.writeDigitImageArray(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), 1, 49, 11, 0, matrix.eTransparent.u, matrix.oled_eFaktor(matrix.eFaktor.f2))
+    matrix.writeDisplay(6, 7, matrix.eI2C.I2C_x3C)
+})
